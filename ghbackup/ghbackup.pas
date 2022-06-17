21752bad33e4155a0dc77b4a478828fc75cb43fc
AKotov-dev <alex_q_2000@mail.ru>
2022-06-17 15:24:47 +0300

    v0.2 - cosmetic edits/events

--- ghbackup/ghbackup.pas
+++ ghbackup/ghbackup.pas
@@ -17,7 +17,7 @@ uses
 
 begin
   RequireDerivedFormResource:=True;
-  Application.Title:='GHBackup-v0.1';
+  Application.Title:='GHBackup-v0.2';
   Application.Scaled:=True;
   Application.Initialize;
   Application.CreateForm(TMainForm, MainForm);
